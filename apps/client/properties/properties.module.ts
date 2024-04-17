f6e1bb589037c3de6894214cdcfdb706a9a8be4b
Nour EL KAHLA <62484447+Nour-EL-KAHLA@users.noreply.github.com>
2024-04-17 22:58:47 +0100

    static nav view

--- apps/client/properties/properties.module.ts
+++ apps/client/properties/properties.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { HomeComponent } from './home/home.component'
-import { CarouselComponent } from './home/carousel/carousel.component'
+import { CarouselComponent } from './home/carousel/carousel.component';
+import { NavbarComponent } from './home/navbar/navbar.component'
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomeComponent, CarouselComponent],
+  declarations: [HomeComponent, CarouselComponent, NavbarComponent],
   exports: [HomeComponent]
 })
 export class PropertiesModule {}
