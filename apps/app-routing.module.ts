bd7f54b0c56562ab8522a199303c3dbb10ecd89d
Nour EL KAHLA <62484447+Nour-EL-KAHLA@users.noreply.github.com>
2024-04-17 15:20:28 +0100

    Carousel + body

--- apps/app-routing.module.ts
+++ apps/app-routing.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core'
-import { mapToCanActivate, RouterModule, type Routes } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { mapToCanActivate, RouterModule, type Routes } from '@angular/router';
 
-import { AppComponent } from './app.component'
-import { AuthGuardService } from '../libs/core-data/src/index'
-import { LoginComponent } from 'libs/core-data/src/lib/ui-login/lib/login/login.component'
-import { HomeComponent } from 'apps/client/properties/home/home.component'
+import { AppComponent } from './app.component';
+import { AuthGuardService } from '../libs/core-data/src/index';
+import { LoginComponent } from 'libs/core-data/src/lib/ui-login/lib/login/login.component';
+import { HomeComponent } from 'apps/client/properties/home/home.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
-  { path: '**', component: AppComponent, canActivate: mapToCanActivate([AuthGuardService]) }
-]
+  {
+    path: '**',
+    component: AppComponent,
+    canActivate: mapToCanActivate([AuthGuardService]),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**.{html,ts}',
-    './libs/core-data/src/lib/ui-login/lib/**'],
+    "./libs/core-data/src/lib/ui-login/lib/**",
+    "./apps/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
