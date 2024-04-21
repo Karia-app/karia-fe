015ae8556c24b10af42efd27de9b2ffdb5d5ca02
Elyes5 <elyes.saddem@etudiant-isi.utm.tn>
2024-04-21 13:42:42 +0200

    FIX_CONFLICTS/ADD_HOME

--- apps/app-routing.module.ts
+++ apps/app-routing.module.ts
@@ -1,21 +1,5 @@
 import { NgModule } from '@angular/core';
 import { mapToCanActivate, RouterModule, type Routes } from '@angular/router';
-
-<<<<<<< HEAD
-import { AppComponent } from './app.component';
-import { AuthGuardService } from '../libs/core-data/src/index';
-import { LoginComponent } from 'libs/core-data/src/lib/ui-login/lib/login/login.component';
-import { HomeComponent } from 'apps/client/properties/home/home.component';
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent },
-  {
-    path: '**',
-    component: AppComponent,
-    canActivate: mapToCanActivate([AuthGuardService]),
-  },
-];
-=======
 import { AppComponent } from './app.component'
 import { AuthGuardService } from '../libs/core-data/src/index'
 import { LoginComponent } from 'libs/core-data/src/lib/login/lib/login.component'
@@ -28,10 +12,13 @@ const routes: Routes = [
   { path: 'forgot', component: ForgotComponent },
   { path: 'home', component: HomeComponent },
   { path: 'property', component:  PropertyComponent},
-  { path: 'register', component:RegisterComponent},
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '**',
+    component: AppComponent,
+    canActivate: mapToCanActivate([AuthGuardService]),
+  },
 ]
->>>>>>> origin/master
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
-    "./libs/core-data/src/lib/ui-login/lib/**",
-    "./apps/**/*.{html,ts}",
-=======
     './src/**.{html,ts}',
     './apps/client/properties/navbar/**',
     './libs/core-data/src/lib/login/lib/**',
     './libs/core-data/src/lib/register/lib/**',
     './libs/core-data/src/lib/ui-forgot-password/lib/**'
->>>>>>> origin/master
   ],
   theme: {
     extend: {},
