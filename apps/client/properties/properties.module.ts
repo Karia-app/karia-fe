528c8e680473ca9c501ee51da32f3fba700b547d
Elyes5 <elyes.saddem@etudiant-isi.utm.tn>
2024-04-25 21:11:11 +0200

    ADD_PROPERTY

--- apps/client/properties/properties.module.ts
+++ apps/client/properties/properties.module.ts
@@ -1,17 +1,16 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { HomeComponent } from './home/home.component'
-import { NavbarComponent } from './navbar/navbar.component'
 import { CarouselComponent } from './home/carousel/carousel.component'
 import { PropertyComponent } from './property/property.component'
 import { CalendarModule } from 'primeng/calendar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatBadgeModule } from '@angular/material/badge';
 import { PropertyDetailsComponent } from './property-details/property-details.component';
 import { MatTabsModule } from '@angular/material/tabs'
 import { AvatarModule } from 'primeng/avatar'
+import { MaterialModule } from 'apps/material.module';
+import { PropertyDialogComponent } from './property-dialog/property-dialog.component'
 @NgModule({
-  imports: [CommonModule,CalendarModule,MatIconModule,MatBadgeModule, MatTabsModule, AvatarModule],
+  imports: [CommonModule,CalendarModule, MatTabsModule, AvatarModule, MaterialModule],
   declarations: [HomeComponent, CarouselComponent,PropertyComponent, PropertyDetailsComponent],
   exports: [HomeComponent,PropertyComponent]
 })
