b9d98da4aa11326494772803abcb8be8a2cf4f88
rami9237 <rami.benromdhane@etudiant-isi.utm.tn>
2024-04-25 22:19:32 +0100

    fix errors on merge

--- apps/client/properties/properties.module.ts
+++ apps/client/properties/properties.module.ts
@@ -1,23 +1,40 @@
-import { CommonModule } from '@angular/common'
-import { NgModule } from '@angular/core'
-import { HomeComponent } from './home/home.component'
-import { CarouselComponent } from './home/carousel/carousel.component'
-import { PropertyComponent } from './property/property.component'
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
+import { PropertyComponent } from './property/property.component';
 import { CalendarModule } from 'primeng/calendar';
 import { PropertyDetailsComponent } from './property-details/property-details.component';
-import { MatTabsModule } from '@angular/material/tabs'
+import { MatTabsModule } from '@angular/material/tabs';
 import { AvatarModule } from 'primeng/avatar';
-import { GoogleMapsComponent } from './google-maps/google-maps.component'
+import { GoogleMapsComponent } from './google-maps/google-maps.component';
 import { MaterialModule } from 'apps/material.module';
-import { PropertyDialogComponent } from './property-dialog/property-dialog.component'
-import { MatBadgeModule } from '@angular/material/badge'
-import { MatIconModule } from '@angular/material/icon'
+import { PropertyDialogComponent } from './property-dialog/property-dialog.component';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker'; // Import MatDatepickerModule
+
 @NgModule({
-  imports: [CommonModule,CalendarModule,MatIconModule,MatBadgeModule, MatTabsModule, AvatarModule],
-  declarations: [HomeComponent, CarouselComponent,PropertyComponent, PropertyDetailsComponent, GoogleMapsComponent],
-  exports: [HomeComponent,PropertyComponent, GoogleMapsComponent]
-  imports: [CommonModule,CalendarModule, MatTabsModule, AvatarModule, MaterialModule],
-  declarations: [HomeComponent, CarouselComponent,PropertyComponent, PropertyDetailsComponent],
-  exports: [HomeComponent,PropertyComponent]
+  imports: [
+    CommonModule,
+    CalendarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatTabsModule,
+    AvatarModule,
+    MatDatepickerModule,
+  ],
+  declarations: [
+    HomeComponent,
+    CarouselComponent,
+    PropertyComponent,
+    PropertyDetailsComponent,
+    GoogleMapsComponent,
+  ],
+  exports: [
+    HomeComponent,
+    PropertyComponent,
+    GoogleMapsComponent
+  ]
 })
-export class PropertiesModule {}
+export class PropertiesModule { }
