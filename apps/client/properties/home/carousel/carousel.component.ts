cd03a20b969b2acc922b843badbf09a23fe3e129
Nour EL KAHLA <62484447+Nour-EL-KAHLA@users.noreply.github.com>
2024-04-26 19:53:39 +0100

    Home wroks

--- apps/client/properties/home/carousel/carousel.component.ts
+++ apps/client/properties/home/carousel/carousel.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class CarouselComponent {
   images: string[] = [
-    'https://img.freepik.com/free-photo/3d-rendering-loft-luxury-living-room-with-bookshelf_105762-2104.jpg?t=st=1713285296~exp=1713288896~hmac=d32f1d4ebc2e556f1b3e87c397dcc29239685043f8c3ab94b2dfcd0e3670a8ab&w=1060',
-    'https://img.freepik.com/free-photo/modern-living-room-style_53876-144814.jpg?t=st=1713286392~exp=1713289992~hmac=ef1e3a5b335f82c75767bf8a63ab48fcbf1cc4f1173a0e451139386a9f2af389&w=1060',
-    'https://img.freepik.com/free-photo/3d-rendering-loft-luxury-living-room-with-bookshelf-near-bookshelf_105762-2224.jpg?t=st=1713286426~exp=1713290026~hmac=ba036f297fb033927f5973fd828e8f31a6c2395126eed23d6595f58991ced402&w=1060',
+    'https://static.zerodown.com/photos/34547752/686794609/original.jpg',
+
+    'https://housing.com/news/wp-content/uploads/2022/11/shutterstock_1715891752-1200x700-compressed.jpg',
+    'https://cdn.houseplansservices.com/product/h4ear534efn58jv3nn8np4vmd9/w800x533.jpg?v=2',
   ];
 
   currentIndex = 0;
--- apps/client/properties/home/home.component.ts
+++ apps/client/properties/home/home.component.ts
@@ -18,9 +18,9 @@ export class HomeComponent {
   thenBlock: TemplateRef<any> | null = null;
   Block: TemplateRef<any> | null = null;
   images: string[] = [
-    'https://img.freepik.com/free-photo/3d-rendering-loft-luxury-living-room-with-bookshelf_105762-2104.jpg?t=st=1713285296~exp=1713288896~hmac=d32f1d4ebc2e556f1b3e87c397dcc29239685043f8c3ab94b2dfcd0e3670a8ab&w=1060',
-    'https://img.freepik.com/free-photo/modern-living-room-style_53876-144814.jpg?t=st=1713286392~exp=1713289992~hmac=ef1e3a5b335f82c75767bf8a63ab48fcbf1cc4f1173a0e451139386a9f2af389&w=1060',
-    'https://img.freepik.com/free-photo/3d-rendering-loft-luxury-living-room-with-bookshelf-near-bookshelf_105762-2224.jpg?t=st=1713286426~exp=1713290026~hmac=ba036f297fb033927f5973fd828e8f31a6c2395126eed23d6595f58991ced402&w=1060',
+    'https://static.vecteezy.com/system/resources/thumbnails/023/308/053/small_2x/ai-generative-exterior-of-modern-luxury-house-with-garden-and-beautiful-sky-photo.jpg',
+    'https://cdn11.bigcommerce.com/s-g95xg0y1db/images/stencil/1280x1280/g/modern%20house%20plan%20-%20carbondale__05776.original.jpg',
+    'https://static.zerodown.com/photos/34547752/686794609/original.jpg',
   ];
 
   currentIndex: number[] = Array(6).fill(0);
